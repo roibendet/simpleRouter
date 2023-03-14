006a550b953cf5e9f383f47ab6daf4afaba79a87
roibWix <roib@wix.com>
2023-03-14 13:24:47 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
+import ReactDOM from "react-dom/client";
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Layout from "./pages/Layout/Layout";
+import Home from "./pages/Home/Home";
+import Blogs from "./pages/Blogs/Blogs";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to={'/'}>
+            home
+            </Link>
+          </li>
+          <li>
+            <Link to={'/blogs'}>
+            blogs
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+      <Routes>
+          <Route index path="/"  element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+      </Routes>
+      <footer>
+        footer
+      </footer>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);
